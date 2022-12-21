79ef6d1a9ac376a7c1a435b155b6f39c204b2952
VadimTalimonchik <tvadmins@gmail.com>
2022-12-21 16:07:16 +0300

    Числа Фибоначчи

--- example13_RecursionAlgorithm/Program.cs
+++ example13_RecursionAlgorithm/Program.cs
@@ -137,17 +137,28 @@
 // PrintImage(pic);    // выводим закрашенную картинку
 
 // РЕКУРСИЯ
-// Факториал
+// // Факториал
 
-// int Factorial(int n)
-// {
-//     if(n == 1) return 1;
-//     else return n * Factorial(n - 1);
-// }
-// Console.WriteLine(Factorial(3));
+// // int Factorial(int n)
+// // {
+// //     if(n == 1) return 1;
+// //     else return n * Factorial(n - 1);
+// // }
+// // Console.WriteLine(Factorial(3));
 
 
-// int Factorial(int n)
+// // int Factorial(int n)
+// // {
+// //     if(n == 1) return 1;
+// //     else return n * Factorial(n - 1);
+// // }
+
+// // for(int i = 1; i < 40; i++)
+// // {
+// //     Console.WriteLine($"{i}! = {Factorial(i)}");
+// // }
+
+// double Factorial(int n)
 // {
 //     if(n == 1) return 1;
 //     else return n * Factorial(n - 1);
@@ -158,13 +169,30 @@
 //     Console.WriteLine($"{i}! = {Factorial(i)}");
 // }
 
-double Factorial(int n)
-{
-    if(n == 1) return 1;
-    else return n * Factorial(n - 1);
-}
+// // Фибоначи
 
-for(int i = 1; i < 40; i++)
+// // f(1) = 1;
+// // f(2) = 2;
+// // f(n) = f(n-1) + f(n-2);
+
+// int Fibonacci(int n)
+// {
+//     if(n == 1 || n == 2) return 1;
+//     else return Fibonacci(n - 1) + Fibonacci(n - 2);
+// }
+
+// for(int i = 1; i < 10; i++)
+// {
+//     Console.WriteLine(Fibonacci(i));
+// }
+
+double Fibonacci(int n)
 {
-    Console.WriteLine($"{i}! = {Factorial(i)}");
+    if(n == 1 || n == 2) return 1;
+    else return Fibonacci(n - 1) + Fibonacci(n - 2);
+}
+// для проверки на каком числе начинает подтормаживать алгоритм
+for(int i = 1; i < 50; i++)
+{
+    Console.WriteLine($"f({i}) = {Fibonacci(i)}");
 }
